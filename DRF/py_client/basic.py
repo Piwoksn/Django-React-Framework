391896523c0ef9f70d3ae0287e944cd73494312b
Piwoksn <piwoksn@gmail.com>
2024-10-25 13:01:26 +0100

    working on http status codes

--- DRF/py_client/basic.py
+++ DRF/py_client/basic.py
@@ -3,9 +3,10 @@ import requests
 endpoint = "https://www.httpbin.org/status/200/"
 endpoint = "https://www.httpbin.org"
 endpoint = "https://www.httpbin.org/anything" #returns a json format (JavaScript Object Notation)
+endpoint = "localhost:8000/" # http://127.0.0.1:8000/
 
 get_responde = requests.get(endpoint) #HTTP Request
 print(get_responde.text) # Prints the source code
-print(get_responde.json()) # Prints the JSON format
+# print(get_responde.json()) # Prints the JSON format
 print(get_responde.status_code) # Prints the status code
 
