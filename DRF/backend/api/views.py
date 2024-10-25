404f8170d788f8a5a7493a1ccf6ffb74c3dab49f
Piwoksn <piwoksn@gmail.com>
2024-10-25 14:32:54 +0100

    NEXT WE WANT TO INVOLVE OR INCLUDE MODELS

--- DRF/backend/api/views.py
+++ DRF/backend/api/views.py
@@ -7,10 +7,17 @@ def api_home(request, *args, **kwargs):
     body = request.body
     data = {}
     try:
-        data = json.loads(body)
+        data = json.loads(body) # takes a string of json data and converts it into python dictionary
     except:
         pass
     
     # print(body)
+    print(data)
     print(data.keys())
-    return JsonResponse({"message": "Hi Dear, this is your django API Response"})
+    print(request.GET)
+    
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    # return JsonResponse({"message": "Hi Dear, this is your django API Response"})
+    return JsonResponse(data)
