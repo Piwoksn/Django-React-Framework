361650f598e2069909486514b72e6dc633935551
Piwoksn <piwoksn@gmail.com>
2024-10-25 14:43:31 +0100

    created another app called products

--- DRF/backend/cfehome/urls.py
+++ DRF/backend/cfehome/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')) # localhost:8000/api/
+    path('api/', include('api.urls')), # localhost:8000/api/
+    path('products/', include('products.urls')),
 ]
