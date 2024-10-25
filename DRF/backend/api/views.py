59580f1702301fb57a476f939bf9be3f18b65e37
Piwoksn <piwoksn@gmail.com>
2024-10-25 20:05:28 +0100

    called my model from my api_home function

--- DRF/backend/api/views.py
+++ DRF/backend/api/views.py
@@ -1,23 +1,33 @@
 from django.http import JsonResponse
 import json
+from products.models import Product
 
 # Create your views here.
 
+# def api_home(request, *args, **kwargs):
+#     body = request.body
+#     data = {}
+#     try:
+#         data = json.loads(body) # takes a string of json data and converts it into python dictionary
+#     except:
+#         pass
+    
+#     # print(body)
+#     print(data)
+#     print(data.keys())
+#     print(request.GET)
+    
+#     data['params'] = dict(request.GET)
+#     data['headers'] = dict(request.headers)
+#     data['content_type'] = request.content_type
+#     # return JsonResponse({"message": "Hi Dear, this is your django API Response"})
+#     return JsonResponse(data)
+
 def api_home(request, *args, **kwargs):
-    body = request.body
+    model_data = Product.object.all().order_by("?").first()
     data = {}
-    try:
-        data = json.loads(body) # takes a string of json data and converts it into python dictionary
-    except:
-        pass
-    
-    # print(body)
-    print(data)
-    print(data.keys())
-    print(request.GET)
-    
-    data['params'] = dict(request.GET)
-    data['headers'] = dict(request.headers)
-    data['content_type'] = request.content_type
-    # return JsonResponse({"message": "Hi Dear, this is your django API Response"})
+    if model_data:
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
     return JsonResponse(data)
